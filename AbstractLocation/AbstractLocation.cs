d758e5313854c0391c91a8f60a7059621e3dc451
annlle <ua.lerus@gmail.com>
2023-10-22 16:59:55 +0300

    add prototype of abstraction location class

---
+++ AbstractLocation/AbstractLocation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace misto
+{
+public abstract class Location
+{
+		protected string adress;
+		protected string geo_location;
+		protected string picture;
+
+	public Location(string adress)
+	{
+		this.adress = adress;
+	}
+
+
+}
+}
