129a9ab04566703aefea892f5d1925e74caa5de9
annlle <ua.lerus@gmail.com>
2023-11-11 10:25:54 +0200

    added RouteTile and EventTile classes

---
+++ Scripts/RouteTile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kursova.Scripts
+{
+    public class RouteTile
+    {
+        public Label Number { get; private set;}
+        public Label Direction { get; private set;}
+        public Label Frequency { get; private set;}
+        public Label Time { get; private set;}
+        public PictureBox TypePicture { get; private set;}
+        public Route Route { get; private set;}
+
+        public RouteTile(Panel template, Label num, Label dir, Label freq, Label time, PictureBox typePic, Route route)
+        {
+
+        }
+
+    }
+}
