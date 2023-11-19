b30e64d6ea1a7639df725370e8af3dba52e70ec2
annlle <ua.lerus@gmail.com>
2023-11-19 21:57:41 +0200

    added Attractrion class

---
+++ Scripts/Attraction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text;
+
+namespace kursova.Scripts
+{ 
+    public class Attraction
+    {
+        public string name { get; set; }
+        public string description { get; set; }
+        public Location location { get; set; }
+        public Image image { get; set; } 
+    }
+}
