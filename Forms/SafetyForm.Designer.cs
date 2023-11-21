18872099ad5a5b0e8945dec7da5822868fbe8f67
millleva <kochevamilenazp@gmail.com>
2023-11-21 23:17:35 +0200

    edit safety and travel forms

--- Forms/SafetyForm.Designer.cs
+++ Forms/SafetyForm.Designer.cs
@@ -49,52 +49,59 @@
             this.locationListBox = new System.Windows.Forms.ListBox();
             this.eventTileTemplate.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.locationLinkTileTemplate)).BeginInit();
+            this.panelDanger.SuspendLayout();
             this.SuspendLayout();
             // 
             // typeLabel
             // 
             this.typeLabel.AutoSize = true;
-            this.typeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.typeLabel.BackColor = System.Drawing.Color.Transparent;
+            this.typeLabel.Font = new System.Drawing.Font("Franklin Gothic Medium", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.typeLabel.Location = new System.Drawing.Point(23, 148);
             this.typeLabel.Name = "typeLabel";
-            this.typeLabel.Size = new System.Drawing.Size(127, 20);
+            this.typeLabel.Size = new System.Drawing.Size(127, 24);
             this.typeLabel.TabIndex = 0;
             this.typeLabel.Text = "Що відбулося:";
             // 
             // backButton
             // 
+            this.backButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(149)))), ((int)(((byte)(164)))));
+            this.backButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("backButton.BackgroundImage")));
+            this.backButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.backButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.backButton.Location = new System.Drawing.Point(14, 12);
             this.backButton.Name = "backButton";
-            this.backButton.Size = new System.Drawing.Size(100, 29);
+            this.backButton.Size = new System.Drawing.Size(65, 60);
             this.backButton.TabIndex = 2;
-            this.backButton.Text = "НАЗАД";
-            this.backButton.UseVisualStyleBackColor = true;
+            this.backButton.UseVisualStyleBackColor = false;
             this.backButton.Click += new System.EventHandler(this.backButton_Click);
             // 
             // levelSafetyLabel
             // 
             this.levelSafetyLabel.AutoSize = true;
-            this.levelSafetyLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.levelSafetyLabel.BackColor = System.Drawing.Color.Transparent;
+            this.levelSafetyLabel.Font = new System.Drawing.Font("Franklin Gothic Medium", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.levelSafetyLabel.Location = new System.Drawing.Point(23, 222);
             this.levelSafetyLabel.Name = "levelSafetyLabel";
-            this.levelSafetyLabel.Size = new System.Drawing.Size(162, 20);
+            this.levelSafetyLabel.Size = new System.Drawing.Size(164, 24);
             this.levelSafetyLabel.TabIndex = 3;
             this.levelSafetyLabel.Text = "Рівень небезпеки:";
             // 
             // descriptionLabel
             // 
             this.descriptionLabel.AutoSize = true;
-            this.descriptionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.descriptionLabel.BackColor = System.Drawing.Color.Transparent;
+            this.descriptionLabel.Font = new System.Drawing.Font("Franklin Gothic Medium", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.descriptionLabel.Location = new System.Drawing.Point(23, 256);
             this.descriptionLabel.Name = "descriptionLabel";
-            this.descriptionLabel.Size = new System.Drawing.Size(56, 20);
+            this.descriptionLabel.Size = new System.Drawing.Size(56, 24);
             this.descriptionLabel.TabIndex = 5;
             this.descriptionLabel.Text = "Опис:";
             // 
             // descriptionTextBox
             // 
-            this.descriptionTextBox.Location = new System.Drawing.Point(189, 256);
+            this.descriptionTextBox.Font = new System.Drawing.Font("Franklin Gothic Book", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.descriptionTextBox.Location = new System.Drawing.Point(189, 255);
             this.descriptionTextBox.Multiline = true;
             this.descriptionTextBox.Name = "descriptionTextBox";
             this.descriptionTextBox.Size = new System.Drawing.Size(166, 145);
@@ -102,7 +109,7 @@
             // 
             // postButton
             // 
-            this.postButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.postButton.Font = new System.Drawing.Font("Franklin Gothic Medium", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.postButton.Location = new System.Drawing.Point(121, 439);
             this.postButton.Name = "postButton";
             this.postButton.Size = new System.Drawing.Size(130, 42);
@@ -113,19 +120,21 @@
             // 
             // typeComboBox
             // 
+            this.typeComboBox.Font = new System.Drawing.Font("Franklin Gothic Book", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.typeComboBox.FormattingEnabled = true;
-            this.typeComboBox.Location = new System.Drawing.Point(189, 147);
+            this.typeComboBox.Location = new System.Drawing.Point(189, 151);
             this.typeComboBox.Name = "typeComboBox";
-            this.typeComboBox.Size = new System.Drawing.Size(166, 21);
+            this.typeComboBox.Size = new System.Drawing.Size(166, 29);
             this.typeComboBox.TabIndex = 8;
             // 
             // locationLabel
             // 
             this.locationLabel.AutoSize = true;
-            this.locationLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.locationLabel.BackColor = System.Drawing.Color.Transparent;
+            this.locationLabel.Font = new System.Drawing.Font("Franklin Gothic Medium", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.locationLabel.Location = new System.Drawing.Point(23, 185);
             this.locationLabel.Name = "locationLabel";
-            this.locationLabel.Size = new System.Drawing.Size(77, 20);
+            this.locationLabel.Size = new System.Drawing.Size(74, 24);
             this.locationLabel.TabIndex = 9;
             this.locationLabel.Text = "Адреса:";
             // 
@@ -166,16 +175,17 @@
             // locationTileTemplate
             // 
             this.locationTileTemplate.BackColor = System.Drawing.Color.Transparent;
+            this.locationTileTemplate.Font = new System.Drawing.Font("Franklin Gothic Medium", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.locationTileTemplate.Location = new System.Drawing.Point(30, 25);
             this.locationTileTemplate.Name = "locationTileTemplate";
-            this.locationTileTemplate.Size = new System.Drawing.Size(215, 30);
+            this.locationTileTemplate.Size = new System.Drawing.Size(215, 35);
             this.locationTileTemplate.TabIndex = 2;
             this.locationTileTemplate.Text = "Космічний житломасив, Запоріжжя, Запорізька область, 69000\r\n";
             // 
             // typeTileTemplate
             // 
             this.typeTileTemplate.BackColor = System.Drawing.Color.Transparent;
-            this.typeTileTemplate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.typeTileTemplate.Font = new System.Drawing.Font("Franklin Gothic Demi", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.typeTileTemplate.Location = new System.Drawing.Point(30, 5);
             this.typeTileTemplate.Name = "typeTileTemplate";
             this.typeTileTemplate.Size = new System.Drawing.Size(215, 21);
@@ -185,35 +195,39 @@
             // safetyListLabel
             // 
             this.safetyListLabel.AutoSize = true;
-            this.safetyListLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.safetyListLabel.Location = new System.Drawing.Point(523, 83);
+            this.safetyListLabel.BackColor = System.Drawing.Color.Transparent;
+            this.safetyListLabel.Font = new System.Drawing.Font("Franklin Gothic Demi", 18F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.safetyListLabel.Location = new System.Drawing.Point(524, 99);
             this.safetyListLabel.Name = "safetyListLabel";
-            this.safetyListLabel.Size = new System.Drawing.Size(141, 24);
+            this.safetyListLabel.Size = new System.Drawing.Size(157, 30);
             this.safetyListLabel.TabIndex = 13;
             this.safetyListLabel.Text = "Список подій";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(104, 83);
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Franklin Gothic Demi", 18F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(101, 99);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(147, 24);
+            this.label1.Size = new System.Drawing.Size(159, 30);
             this.label1.TabIndex = 14;
             this.label1.Text = "Додати подію";
             // 
             // panelDanger
             // 
-            this.panelDanger.Location = new System.Drawing.Point(189, 222);
+            this.panelDanger.Controls.Add(this.locationListBox);
+            this.panelDanger.Location = new System.Drawing.Point(189, 226);
             this.panelDanger.Name = "panelDanger";
             this.panelDanger.Size = new System.Drawing.Size(166, 20);
             this.panelDanger.TabIndex = 15;
             // 
             // locationTextBox
             // 
-            this.locationTextBox.Location = new System.Drawing.Point(189, 185);
+            this.locationTextBox.Font = new System.Drawing.Font("Franklin Gothic Book", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.locationTextBox.Location = new System.Drawing.Point(189, 189);
             this.locationTextBox.Name = "locationTextBox";
-            this.locationTextBox.Size = new System.Drawing.Size(166, 20);
+            this.locationTextBox.Size = new System.Drawing.Size(166, 26);
             this.locationTextBox.TabIndex = 16;
             this.locationTextBox.TextChanged += new System.EventHandler(this.locationTextBox_TextChanged);
             this.locationTextBox.Leave += new System.EventHandler(this.locationTextBox_Leave);
@@ -221,9 +235,9 @@
             // locationListBox
             // 
             this.locationListBox.FormattingEnabled = true;
-            this.locationListBox.Location = new System.Drawing.Point(189, 207);
+            this.locationListBox.Location = new System.Drawing.Point(0, -4);
             this.locationListBox.Name = "locationListBox";
-            this.locationListBox.Size = new System.Drawing.Size(166, 43);
+            this.locationListBox.Size = new System.Drawing.Size(166, 30);
             this.locationListBox.TabIndex = 17;
             this.locationListBox.Visible = false;
             this.locationListBox.SelectedIndexChanged += new System.EventHandler(this.locationListBox_SelectedIndexChanged);
@@ -232,8 +246,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(808, 537);
-            this.Controls.Add(this.locationListBox);
             this.Controls.Add(this.locationTextBox);
             this.Controls.Add(this.panelDanger);
             this.Controls.Add(this.label1);
@@ -248,12 +263,14 @@
             this.Controls.Add(this.levelSafetyLabel);
             this.Controls.Add(this.backButton);
             this.Controls.Add(this.typeLabel);
+            this.DoubleBuffered = true;
             this.Name = "SafetyForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Safety";
             this.Load += new System.EventHandler(this.SafetyForm_Load);
             this.eventTileTemplate.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.locationLinkTileTemplate)).EndInit();
+            this.panelDanger.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- Forms/TravelInfoForm.Designer.cs
+++ Forms/TravelInfoForm.Designer.cs
@@ -52,44 +52,51 @@
             // 
             // attractionTextLabel
             // 
-            this.attractionTextLabel.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.attractionTextLabel.Location = new System.Drawing.Point(22, 112);
+            this.attractionTextLabel.BackColor = System.Drawing.Color.Transparent;
+            this.attractionTextLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.attractionTextLabel.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.attractionTextLabel.Location = new System.Drawing.Point(12, 103);
             this.attractionTextLabel.Name = "attractionTextLabel";
             this.attractionTextLabel.Size = new System.Drawing.Size(355, 302);
             this.attractionTextLabel.TabIndex = 1;
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(132)))), ((int)(((byte)(255)))), ((int)(((byte)(207)))));
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(110, 49);
+            this.button1.Size = new System.Drawing.Size(65, 60);
             this.button1.TabIndex = 2;
-            this.button1.Text = "НАЗАД";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.backButton_Click);
             // 
             // sightLabel
             // 
             this.sightLabel.AutoSize = true;
-            this.sightLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.sightLabel.Location = new System.Drawing.Point(161, 51);
+            this.sightLabel.BackColor = System.Drawing.Color.Transparent;
+            this.sightLabel.Font = new System.Drawing.Font("Franklin Gothic Demi", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.sightLabel.Location = new System.Drawing.Point(219, 44);
             this.sightLabel.Name = "sightLabel";
-            this.sightLabel.Size = new System.Drawing.Size(159, 20);
+            this.sightLabel.Size = new System.Drawing.Size(158, 24);
             this.sightLabel.TabIndex = 3;
             this.sightLabel.Text = "Оберіть пам\'ятку:";
             // 
             // attractionsComboBox
             // 
+            this.attractionsComboBox.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.attractionsComboBox.FormattingEnabled = true;
-            this.attractionsComboBox.Location = new System.Drawing.Point(326, 51);
+            this.attractionsComboBox.Location = new System.Drawing.Point(383, 43);
             this.attractionsComboBox.Name = "attractionsComboBox";
-            this.attractionsComboBox.Size = new System.Drawing.Size(287, 21);
+            this.attractionsComboBox.Size = new System.Drawing.Size(287, 29);
             this.attractionsComboBox.TabIndex = 4;
             this.attractionsComboBox.SelectedIndexChanged += new System.EventHandler(this.attractionsComboBox_SelectedIndexChanged);
             // 
             // locationLinkPictureBox
             // 
+            this.locationLinkPictureBox.BackColor = System.Drawing.Color.Transparent;
             this.locationLinkPictureBox.Cursor = System.Windows.Forms.Cursors.Hand;
             this.locationLinkPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("locationLinkPictureBox.Image")));
             this.locationLinkPictureBox.Location = new System.Drawing.Point(728, 12);
@@ -104,6 +111,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.locationLinkPictureBox);
             this.Controls.Add(this.attractionsComboBox);
@@ -111,6 +120,7 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.attractionTextLabel);
             this.Controls.Add(this.attractionPictureBox);
+            this.DoubleBuffered = true;
             this.Name = "TravelInfoForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Travel Info";
